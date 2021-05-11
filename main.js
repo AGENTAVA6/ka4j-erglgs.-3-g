0718da58cee8cb7477c352ba51b8af8ee24a7aa2
AGENTAVA6 <72601590+AGENTAVA6@users.noreply.github.com>
2021-05-11 16:00:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function setup(){
+    canvas = createCanvas(300,300)
+    video  = createCapture(VIDEO)
+ video.hide()
+ canvas.center()
+ video.size(300,300)
+ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/u_BkFWfU1/model.json',modelLoaded);
+}
+function draw(){
+    image(video, 0 ,0, 300, 300)
+    classifier.classify(video,gotResult)
+}
+function modelLoaded(){
+    console.log("model loaddded")
+}
+function modelLoaded(){
+    console.log("modellllllllllllllll")
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results)
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+    }
